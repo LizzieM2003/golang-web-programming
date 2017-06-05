112cafa26ed67c6bed5db60f6cecd916c82861fc
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-06-05 13:43:11 +0100

    Add predefined global functions in templates chapter

---
+++ 07_predefinedGlobalFunctions/01_index/02/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	xs := []string{"zero", "one", "two", "three", "four"}
+
+	data := struct {
+		Words []string
+		LName string
+	}{
+		Words: xs,
+		LName: "Mendes",
+	}
+
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
